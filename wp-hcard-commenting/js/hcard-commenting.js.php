73bdd39f3f0f73fd35a8e2838b00a17e7e54cc69
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-15 20:29:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hcard-commenting/tags/0.6.1@34986 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-hcard-commenting/js/hcard-commenting.js.php
@@ -0,0 +1,62 @@
+<?php
+require_once('./../../../../wp-config.php');
+
+header('Content-Type: application/javascript; charset=utf-8');
+?>
+
+jQuery(document).ready( function() {
+  jQuery('#commentform').addClass('hcard-commenting');
+
+  var html = ' <a id="hcard_enabled_link" href="http://microformats.org/wiki/hCard">(import hCard)</a> '
+           + ' <span id="ajax-loader" style="display: none;">Loading hCard</span> ';
+  
+  if (jQuery('#commentform a[@id=hcard_enabled_link]').length == 0) {
+    jQuery('#commentform input[@id=url]').next().append(html);
+  }
+  
+  jQuery('#hcard_enabled_link').click( function() {
+    var url = jQuery("#commentform input[@id=url]").val();
+    if (url == "") {
+      jQuery("#commentform input[@id=url]").val("use this field for your hCard url");
+    } else {
+      jQuery.ajax({
+        type: "GET",
+        url: "<?php echo parse_url(get_option('siteurl'), PHP_URL_PATH); ?>/index.php",
+        data: {hcard_url: url},
+        dataType: "json",
+        timeout: 10000,
+        error: function(XMLHttpRequest, textStatus, errorThrown) { alert(textStatus + ": " + errorThrown); },
+        success: function(data) {
+          if (data.vcard.fn != "") {
+            jQuery("#commentform input[@id=author]").val(data.vcard.fn);
+          }
+          if (data.vcard.email != "") {
+            jQuery("#commentform input[@id=email]").val(data.vcard.email);
+          }
+          if (data.vcard.url != "") {
+            jQuery("#commentform input[@id=url]").val(data.vcard.url);
+          }
+        }
+      });
+    }
+      
+    return false;
+  });
+
+  jQuery("#ajax-loader").ajaxStart(function(){
+    jQuery(this).show();
+    jQuery('#hcard_enabled_link').hide();
+  });
+
+  jQuery("#ajax-loader").ajaxStop(function(){
+    jQuery(this).hide();
+    jQuery('#hcard_enabled_link').show();
+  });
+
+  jQuery('#commentform input[@id=url]').click( function() {
+    var url = jQuery("#commentform input[@id=url]").val();
+    if (url == "use this field for your hCard url") {
+      jQuery("#commentform input[@id=url]").val("");
+    }
+  });
+});
---
+++ wp-hcard-commenting/readme.txt
@@ -0,0 +1,49 @@
+=== Plugin Name ===
+Contributors: pfefferle
+Donate link: http://notizblog.org
+Tags: hCard Commenting, hCard, OpenID, Auth, Commenting, Comments, Microformats
+Requires at least: 2.2.x
+Tested up to: 2.3.3
+Stable tag: 0.6.1
+
+This Plugin allows your users to easily fill out your comment forms using an hCard.
+
+== Description ==
+
+This Plugin allows your users to easily fill out your comment forms using hCard. I got this
+Idea from SignUp pages like [bragster.com](https://www.bragster.com/signup) or 
+[getsatisfaction.com](http://getsatisfaction.com/people/new).
+
+hCard-Commenting now supports [representative hCards](http://microformats.org/wiki/representative-hcard)
+
+== Installation ==
+
+* Upload the `wp-hcard-commenting` to your **plugin** folder
+
+* It should work for the most themes (like K2 or Kubrik) without changes
+
+* For all others, simply add `<?php hcard_commenting_link() ?>` where you want
+the link to be displayed
+
+Thats it
+
+== Frequently Asked Questions ==
+
+== Which Microformats parser du you use? ==
+
+I use the [hKit](http://allinthehead.com/hkit/) parser from Drew McLellan and the hKit
+service from [microformatic.com](http://tools.microformatic.com/help/xhtml/hkit/).
+
+hCard-Commenting supports [representative hCards](http://microformats.org/wiki/representative-hcard)
+
+== Why do you also use the hKit service ==
+
+hKit needs PHP5, if you run PHP4.3 you can't run the hKit parser natively.
+
+== What are the next steps? ==
+
+Implementing some kind of avatar service like hAvatar.
+
+== Screenshots ==
+
+You can find a demo here: [notizBlog.org](http://notizblog.org).
